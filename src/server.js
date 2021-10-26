92404df385b1efe92db29154c7aae24292e2e6e9
oguzhan <oguzhantekeli@gmail.com>
2021-10-26 21:59:37 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
-import express from 'express';
+const express = require("express");
+const axios = require("axios");
 
-const server=()=>{
-    return true;
-}
+const app = express();
+app.get("/", (req, res) => {});
+app.listen(3000, () => console.log("server started"));
+
+const baseUrl = "api.openweathermap.org/data/2.5/weather?";
+const apiKey = "22def37a278a366a676db77452b70402";
+
+export const getWeatherData = async (cityName) => {
+  try {
+    const { data } = await axios.get(`${baseUrl}q=${cityName}&appid=${apiKey}`);
+    return data;
+  } catch (e) {
+    throw e;
+  }
+};
